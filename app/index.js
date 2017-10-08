0b2b1ed585915a968fc0b01b929680908ab2b104
ccanales.b <canaleschiko@gmail.com>
2017-10-08 16:18:13 -0300

    first commit

---
+++ app/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View
+  } from 'react-native';
+export default class FurgonerAttorney extends Component {
+    render(){
+        return (
+            <View>
+                <Text>
+                    hola
+                </Text>
+            </View>
+
+        )
+    }
+}
