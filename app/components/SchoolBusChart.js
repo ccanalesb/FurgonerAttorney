d96d1107bfbbae07cb555a4e56d2c01804011fa9
ccanalesbarra <cristian@devaid.cl>
2017-12-03 20:50:16 -0300

    mostrando mapas en android e ios

--- app/components/SchoolBusChart.js
+++ app/components/SchoolBusChart.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, ScrollView, Platform } from 'react-native';
 import Spinner from 'react-native-loading-spinner-overlay';
 import { firebaseRef } from '../services/firebase.js'
 import { VictoryChart, 
@@ -9,8 +9,9 @@ import { VictoryChart,
     VictoryZoomContainer, 
     VictoryBrushContainer,
     VictoryGroup,
-    VictoryScatter } from "victory-native";
-
+    VictoryScatter,
+    VictoryTheme } from "victory-native";
+import { DatePicker, List, InputItem, Button, Card } from 'antd-mobile';    
 
 export default class SchoolBus extends Component {
     constructor(props) {
@@ -23,7 +24,18 @@ export default class SchoolBus extends Component {
             z_data: [],
             tickValues: [],
             min: 0,
-            max: 0
+            max: 0,
+            day: 'someday',
+            days:[],
+            suddenness: [
+                { x: 1, y: 0, label:'Sunday'},
+                { x: 2, y: 0, label: 'Monday'},
+                { x: 3, y: 0, label: 'Tuesday'},
+                { x: 4, y: 0, label: 'Wednesday'},
+                { x: 5, y: 0, label: 'Thursday'},
+                { x: 6, y: 0, label: 'Friday'},
+                { x: 7, y: 0, label: 'Saturday'},
+            ]
         };
     }
     formatTime(time_to_show){
@@ -33,9 +45,11 @@ export default class SchoolBus extends Component {
     }
     componentWillMount(){
         console.log("Montando componente")
+        
         let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
         var d = new Date();
         let day = days[d.getDay()];
+        this.setState({day: day, days: days})
         var user = firebaseRef.auth().currentUser;
         let temp_x_data = this.state.x_data
         let temp_y_data = this.state.y_data
@@ -74,6 +88,29 @@ export default class SchoolBus extends Component {
         //     <AttorneysCard key={i} attorney={e} type={this.props.attorney_status} />
         // )
     }
+    componentDidMount(){
+        this.get_suddenness()
+    }
+    get_suddenness(){
+        var user = firebaseRef.auth().currentUser;
+        firebaseRef.database().ref('School_bus/' + user.photoURL + '/stadistic/this_week').once("value")
+            .then((snapshot) => {
+                days = snapshot.val()
+                let suddenness = this.state.suddenness
+                for (const [index,value] of this.state.days.entries()) {                    
+                    days[value].map((e,i)=>{
+                        avg = (parseInt(e["X"]) + parseInt(e["Y"]) + parseInt(e["Z"]))/3
+                        if (avg > 50){
+                            points = suddenness[index].y
+                            points = points + 1
+                            suddenness[index].y = points
+                        }
+                    })
+                }
+                this.setState({ suddenness: suddenness })
+            })
+            console.log(this.state)
+    }
     handlePress(){
         var user = firebaseRef.auth().currentUser;
         console.log(user)
@@ -90,78 +127,105 @@ export default class SchoolBus extends Component {
         return (
             <View style={styles.container}>
                 <ScrollView>
-
-                    <VictoryChart 
-                        // padding={{ top: 0, left: 50, right: 50, bottom: 10 }}
-                        width={350} height={320} scale={{ x: "time" }}
-                        domain={{ y: [0, 100] }}
-                        containerComponent={
-                            <VictoryZoomContainer responsive={false}
-                                zoomDimension="x"
-                                zoomDomain={this.state.zoomDomain}
-                                onZoomDomainChange={this.handleZoom.bind(this)}
-                            />}
-                        // containerComponent={<VictoryZoomContainer zoomDomain={{ x: [this.state.min, this.state.max], y: [0, 200] }} />}
-                        
-                        // containerComponent={
-                        //     <VictoryZoomContainer responsive={false}
-                        //         zoomDimension="x"
-                        //         zoomDomain={this.state.zoomDomain}
-                        //         onZoomDomainChange={this.handleZoom.bind(this)}
-                        //     />
-                        // }
-                    >
-                        <VictoryAxis
-                            scale={{ x: "time" }}
-                            label="Hora (HH:MM)"
-                            // tickValues specifies both the number of ticks and where
-                            // they are placed on the axis
-                            // tickValues={[1, 2, 3, 4]}
-                            // tickFormat={["Quarter 1", "Quarter 2", "Quarter 3", "Quarter 4"]}
+                    <Card >
+                        <Card.Header
+                            title={"Comportamiento de manejo del día "+ this.state.day}
                         />
-                        <VictoryGroup
-                            colorScale={["red", "blue", "gold"]}
+                        <Card.Body>
+                            {this.state.x_data.length== 0 || this.state.y_data ==0 || this.state.x_data ==0? null :
+                            <VictoryChart
+                                padding={{ top: 10, left: 30, right: 30, bottom: 100 }}
+                                width={350} height={320} 
+                                scale={{ x: "time" }}
+                                domain={{ y: [0, 100] }}
+                                containerComponent={
+                                    <VictoryZoomContainer responsive={false}
+                                        zoomDimension="x"
+                                        zoomDomain={this.state.zoomDomain}
+                                        onZoomDomainChange={this.handleZoom.bind(this)}
+                                    />}
                             >
-                            <VictoryLine
-                                style={{
-                                    data: { stroke: "tomato" }
-                                }}
-                                data={this.state.x_data}
-                            />
-                            <VictoryLine
-                                data={this.state.y_data}
-                            />
-                            <VictoryLine
-                                data={this.state.z_data}
-                            />
-                        </VictoryGroup>
-                    </VictoryChart>
-                    <Text>
-                        {"\n"}
-                    </Text>    
-                    <VictoryChart
-                        padding={{ top: 0, left: 30, right: 30, bottom: 30 }}
-                        width={320} height={90} scale={{ x: "time" }}
-                        containerComponent={
-                            <VictoryBrushContainer responsive={false}
-                                brushDimension="x"
-                                brushDomain={this.state.selectedDomain}
-                                onBrushDomainChange={this.handleBrush.bind(this)}
-                            />
-                        }
-                    >
-                        <VictoryAxis
-                            tickValues={this.state.tickValues}
-                            tickFormat={(x) => new Date(x).getFullYear()}
-                            label="Hora (HH:MM)"
+                                <VictoryAxis
+                                    scale={{ x: "time" }}
+                                    label="Hora (HH:MM)"
+                                />
+                                <VictoryGroup
+                                    colorScale={["red", "blue", "gold"]}
+                                >
+                                    <VictoryLine
+                                        style={{
+                                            data: { stroke: "tomato" }
+                                        }}
+                                        data={this.state.x_data}
+                                    />
+                                    <VictoryLine
+                                        data={this.state.y_data}
+                                    />
+                                    <VictoryLine
+                                        data={this.state.z_data}
+                                    />
+                                </VictoryGroup>
+                            </VictoryChart>
+                            }
+                            <Text>
+                                {"\n"}
+                            </Text>
+                            {this.state.x_data.length == 0 ? null :
+                            <VictoryChart
+                                padding={{ top: 0, left: 60, right: 60, bottom: 60 }}
+                                width={320} height={90} 
+                                scale={{ x: "time" }}
+                                containerComponent={
+                                    <VictoryBrushContainer responsive={false}
+                                        brushDimension="x"
+                                        brushDomain={this.state.selectedDomain}
+                                        onBrushDomainChange={this.handleBrush.bind(this)}
+                                    />
+                                }
+                            >
+                                <VictoryAxis
+                                    tickValues={this.state.tickValues}
+                                    tickFormat={(x) => new Date(x).getFullYear()}
+                                    label="Hora (HH:MM)"
+                                />
+                                <VictoryLine
+                                    style={{
+                                        data: { stroke: "blue" }
+                                    }}
+                                    data={this.state.x_data}
+                                />
+                            </VictoryChart>
+                            }
+                        </Card.Body>
+                        <Card.Footer content="Este es el comportamiento de manejo del conductor en el dia actual" />
+                    </Card>
+                    <Card >
+                        <Card.Header
+                            title={"Brusquedad de manejo"}
                         />
-                        <VictoryLine
-                            style={{
-                                data: { stroke: "blue" }
-                            }}
-                            data={this.state.x_data}
-                        />
-                    </VictoryChart>
+                        <Card.Body>
+
+                            {
+                                this.state.suddenness.length == 0  ? null :
+                                    <VictoryChart
+                                        theme={VictoryTheme.material}
+                                        domainPadding={{ x: 0 }}
+                                        domain={{ x: [0.5, 7.5]}}
+                                    >
+                                        <VictoryBar
+                                            categories={{
+                                                x: ['L','M','MI','J','V','S','D']
+                                            }}
+                                            style={{ data: { fill: "#c43a31" } }}
+                                            data={this.state.suddenness}
+                                        />
+                                    </VictoryChart>
+                            }
+ 
+                            {/* </VictoryChart> */}
+                        </Card.Body>
+                        <Card.Footer content="Este es un aproximado del comportamiento del conductor por día de la semana" />
+                    </Card>
                     {/* <TouchableOpacity style={styles.buttonContainer} onPress={this.handlePress.bind(this)}>
                         <Text style={styles.buttonText}>
                             mostrar usuario
